dcb25816bb6d403adfa165276dfd762da652718b
Roman <rubin3215@gmail.com>
2024-03-19 01:44:53 +0300

    char charv%ar1 ='A ' нерпавильно считывает на лексемы

--- Lexical_Analyzer_in_C++/Lexical_Analyzer_in_C++.cpp
+++ Lexical_Analyzer_in_C++/Lexical_Analyzer_in_C++.cpp
@@ -12,19 +12,49 @@ int main()
 	vector<string> tokens =
 	{
 	"(", ")", "[", "]", "{", "}",
-	"int", "double", "char", "string",
-	"\"", "\'", ".", ",", "//", ";", 
+	"\"", "\'", ".", ",", ";", 
 	"=", "+", "-", "*", "/", "%",  ">", "<",  "|", "&", "!",
 	"<<", ">>", "||", "&&","++", "--", ">=", "<=", "==", "!=", "/=", "+=", "-=", "%=", "*=",
+	"int", "double", "char", "string"
 	};
-	//16 - 26, 27 - 41
+	// 0 - 5, 6 - 10, 11 - 21, 22 - 36, 37 - 40
+	// a[0-1], a[2-3], a[4-5], a[6-7], a[8-9]
+	vector<char> valid_characters =
+	{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e',
+		'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j',
+		'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
+		'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't',
+		'U', 'u', 'V', 'v', 'W', 'w', 'x', 'x', 'Y', 'y',
+		'Z', 'z', '_'
+	};
+	// 0 - 9, 10 - 61, 62
+	// a[10-11], a[12-13], a[14]
+	vector<string> keywords =
+	{
+	"asm", "double", "new", "switch",
+	"auto", "else", "operator", "template",
+	"break", "enum", "private", "this",
+	"case",	"extern", "protected", "throw",
+	"catch", "float", "publin",	"try"
+	"char",	"for", "register", "typedef"
+	"class", "friend", "return", "union",
+	"const", "goto",	"short", "unsigned",
+	"continue",	"if", "signed",	"virtual",
+	"default", "inline", "sizeof", "void",
+	"delete", "int", "static", "volatile",
+	"do", "long", "struct",	"while"
+	};
+
+	int a[15] = { 0, 5, 6, 10, 11, 21, 22, 36, 37, 40, 0, 9, 10, 61, 62 };
 
 	bool utification = false;
 	vector<string> lexemes, space;
 	vector<char> code, copycode;
 	string lexeme, str, end = "end";
 
-	cout << "Enter your code (to terminate, enter 'end'.):\n\n";
+	cout << "Введите ваш код (для завершения введите end):\n\n";
 	
 	// записал код посимвольно в code
 	while (str != end)
@@ -75,10 +105,11 @@ int main()
 	code.clear();
 	for (int i = 0; i < copycode.size(); i++)
 		code.push_back(copycode[i]);
+	copycode.clear();
 	#pragma endregion
 
 	// нахожу лексемы
-	for (int i = 0; i < code.size(); i++) 
+	for (int i = 0; i < code.size(); i++)
 	{
 		if ((code[i] != ' ') && (code[i] != '\n'))
 		{
@@ -87,10 +118,10 @@ int main()
 			{
 				if (lexeme == tokens[n])
 				{
-					if ((n > 15) && (n < 27) && (i + 1 < code.size()))
+					if ((n >= a[4]) && (n <= a[5]) && (i + 1 < code.size()))
 					{
 						string x = string(1, code[i]) + string(1, code[i + 1]);
-						for (int t = 27; t < 42; t++)
+						for (int t = a[6]; t <= a[7]; t++)
 						{
 							if (x == tokens[t])
 							{
@@ -110,20 +141,22 @@ int main()
 						}
 						else utification = false;
 					}
-					else 
+					else
 					{
 						lexemes.push_back(tokens[n]);
 						lexeme = "";
 						break;
 					}
-					
+
 				}
-				else if (lexeme.find(tokens[n]) != std::string::npos)
-				{ 
-					if ((n > 15) && (n < 27) && (i + 1 < code.size()))
+				else if ((lexeme.find(tokens[n]) != std::string::npos) && !(((lexemes.back() == "int") || (lexemes.back() == "double") || 
+					(lexemes.back() == "char") || (lexemes.back() == "string")) &&
+					!((code[i] == '(') || (code[i] == '[') || (code[i] == '{') || (code[i] == ';'))))
+				{
+					if ((n >= a[4]) && (n <= a[5]) && (i + 1 < code.size()))
 					{
 						string x = string(1, code[i]) + string(1, code[i + 1]);
-						for (int t = 27; t < 42; t++)
+						for (int t = a[6]; t <= a[7]; t++)
 						{
 							if (x == tokens[t])
 							{
@@ -161,18 +194,112 @@ int main()
 		}
 		else
 		{
-			if ((lexeme != ""))
-				lexemes.push_back(lexeme);
-			if (code[i] == '\n')
-				lexemes.push_back("\n");	
+			if (lexeme != "") lexemes.push_back(lexeme);
+			if (code[i] == '\n') lexemes.push_back("\n");
 			lexeme = "";
 		}
 	}
-	
-	cout << "-------------\n";
+
+	cout << "-----------------------------------------------------------\n";
 	for (int i = 0; i < lexemes.size(); i++)
 		if (lexemes[i] != "\n")
 			cout << lexemes[i] << " ";
 		else cout << endl;
+
+
+	int brackets[6] = { 0, 0, 0, 0, 0, 0 };
+	// ()[]{};
+	for (int i = 0; i < lexemes.size(); i++)
+	{
+		for (int n = 0; n < 6; n++)
+		{
+			if (lexemes[i] == tokens[n])
+				brackets[n]++;
+		}
+	}
+
+	cout << "\n-----------------------------------------------------------\n\n";
+
+	//проверяю соответствие открывающихся и закрывающихся скобок
+	if (brackets[0] != brackets[1])
+		cout << "Кол-во открывающихся круглых скобок не соответствует закрывающимся: " << brackets[0] << " '(' и " << brackets[1] << " ')'" << endl;
+	if (brackets[2] != brackets[3])
+		cout << "Кол-во открывающихся квадратных скобок не соответствует закрывающимся: " << brackets[2] << " '[' и " << brackets[3] << " ']'" << endl;
+	if (brackets[4] != brackets[5])
+		cout << "Кол-во открывающихся фигурных скобок не соответствует закрывающимся: " << brackets[4] << " '{' и " << brackets[5] << " '}'" << endl;
+	
+	// проверяю допустимое ли имя идентификатора
+	int lines = 1;
+	for (int i = 0; i < lexemes.size(); i++)
+	{
+		if (lexemes[i] == "\n")
+		{
+			lines++;
+			continue;
+		}
+		for (int n = a[8]; n <= a[9]; n++)
+		{
+			if ((lexemes[i] == tokens[n]) && (i + 1 < lexemes.size()))
+			{
+				string x = lexemes[i + 1];
+				if ((x[0] == '0') || (x[0] == '1') || (x[0] == '2') || (x[0] == '3') || (x[0] == '4') ||
+					(x[0] == '5') || (x[0] == '6') || (x[0] == '7') || (x[0] == '8') || (x[0] == '9'))
+				{
+					cout << "Идентификатор не должен начинаться с цифры, строка " << lines << endl;
+					n = a[9] + 1;
+					break;
+				}
+				if (find(keywords.begin(), keywords.end(), x) != keywords.end())
+				{
+					cout << "Идентификатор не должен совпадать с ключевыми словами, строка " << lines << endl;
+					n = a[9] + 1;
+					break;
+				}
+				for (int m = 0; m < x.length(); m++)
+				{
+					if (find(valid_characters.begin(), valid_characters.end(), x[m]) == valid_characters.end())
+					{
+						cout << "Недопустимые символы в идентификаторе, строка " << lines << endl;
+						n = a[9] + 1;
+						m = x.length();
+						break;
+					}
+				}
+				i++;
+			}
+		}
+	}
+
+	// точка с запятой в строке с cout cin = /= += -= *= %= ++ -- break return continue
+	lines = 1;
+	bool semicolon = false;
+	for (int i = 0; i < lexemes.size(); i++)
+	{
+		if ((lexemes[i] == "cout") || (lexemes[i] == "cin") || (lexemes[i] == "=") || (lexemes[i] == "+=") ||
+			(lexemes[i] == "-=") || (lexemes[i] == "*=") || (lexemes[i] == "/=") || (lexemes[i] == "%n") ||
+			(lexemes[i] == "++") || (lexemes[i] == "--") || (lexemes[i] == "break") || (lexemes[i] == "return") || (lexemes[i] == "continue"))
+		{
+			for (i; i < lexemes.size(); i++)
+			{
+				if ((lexemes[i] == "\n") || (i + 1 == lexemes.size()))
+				{
+					cout << "Необходима точка с запятой, строка " << lines << endl;
+					lines++;
+					break;
+				}
+				if (lexemes[i] == ";")
+				{
+					semicolon = true;
+					break;
+				}
+			}
+		}
+		else if (lexemes[i] == "\n")
+		{
+			semicolon = false;
+			lines++;
+		}
+	}
+
 	return 0;
 }
